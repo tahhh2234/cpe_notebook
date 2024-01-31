09e0a87502c1fcfdfbda9b2c568bbce42b936adc
tahhh2234 <thadpongsermsook1@gmail.com>
2024-01-31 20:32:07 +0700

    debug fix

--- notebook/settings.py
+++ notebook/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == "True"
+DEBUG = os.getenv('DEBUG') == "False"
 
 production_host = os.getenv('PRODUCTION_HOST')
 allowed_hosts_str = os.environ.get("ALLOWED_HOSTS", "")
